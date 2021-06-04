1e2172190b02c4b50131d3deca9c0846b5344a13
sydgroev <Sven.Degroeve@UGent.be>
2021-06-04 18:46:17 +0200

    added Landrick

--- cli.py
+++ cli.py
@@ -5,6 +5,15 @@ from lorikeet.data_parser import get_spectrum_with_mgf, get_varmods
 from flask import Flask
 from flask.templating import render_template
 
+def get_varmods(sequence, modifications):
+    if modifications == "0|":
+        return "[]"
+    mods = "["
+    tmp = modifications.split("|")
+    for i in range(0,len(tmp),2):
+        mods += "{index: %s, modMass: %s, aminoAcid: '%s'}"%(tmp[i],tmp[i+1],sequence[int(tmp[i])-1])
+    return mods + "]"
+
 template_folder = ''
 if getattr(sys, 'frozen', False):
     template_folder = os.path.join(sys._MEIPASS, 'templates')
@@ -20,23 +29,25 @@ def before_first_request():
 
 @app.route('/')
 def main_route():
-    return 'hello'
+    return render_template('landrick copy.html')
 
 @app.route("/sequence/<name>")
 def getSpectrumBySequence(name=None):
     print(name + '.html')
     return render_template(name + '.html')
 
-@app.route("/mgf/<mgf_name>/spectrum/<spectrum_title>/sequence/<seqname>")
-def getSpectrum(mgf_name=None, seqname=None, spectrum_title=None):
+@app.route("/mgf/<mgf_name>/spectrum/<spectrum_title>/sequence/<sequence>/mods/<mods>")
+def getSpectrum(mgf_name=None, sequence=None, spectrum_title=None, mods=None):
     print(sys.argv[1])
     mgf_file_dir = sys.argv[1]
+    modifications = get_varmods(sequence, mods)
     spectrum, charge, parent_mz = get_spectrum_with_mgf(os.path.join(mgf_file_dir,mgf_name), spectrum_title)
     return render_template('spectrum_viewer.html',
         spectrum = spectrum,
         charge = charge,
         parent_mz = parent_mz,
-        sequence = seqname,
+        sequence = sequence,
+        mods = modifications,
         spectrum_title = spectrum_title
         )
 
