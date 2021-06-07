c93cb31141582f2060d4e31468f7da2a4ae1e845
sydgroev <Sven.Degroeve@UGent.be>
2021-06-07 17:14:18 +0200

    wrapped everything up

--- cli.py
+++ cli.py
@@ -1,5 +1,9 @@
 import sys
 import os
+import webbrowser
+from threading import Timer
+
+from flask.helpers import url_for
 #from lorikeet.__main__ import main
 from lorikeet.data_parser import get_spectrum_with_mgf, get_varmods
 from flask import Flask, request
@@ -30,8 +34,21 @@ def before_first_request():
         print('stuff')
 
 @app.route('/')
-def main_route():
-    return render_template('landrick copy.html')
+def tableau():
+    global mgf_file_dir
+    if mgf_file_dir == "":
+        return render_template('tableau.html')
+    else:
+        res = "Tableau is connected to folder '%s'."%mgf_file_dir
+        return render_template('tableau.html',response=res,state="alert-success")
+
+@app.route('/landing')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
 
 @app.route('/setfolder', methods = ['GET', 'POST'])
 def setfolder():
@@ -39,18 +56,18 @@ def setfolder():
    if request.method == 'POST':
       f = request.form['folder']
       mgf_file_dir = f
-      return render_template('landrick copy.html',response=f)
-
-@app.route("/sequence/<name>")
-def getSpectrumBySequence(name=None):
-    print(name + '.html')
-    return render_template(name + '.html')
+      res = "Tableau is connected to folder '%s'."%mgf_file_dir
+      return render_template('tableau.html',response=res,state="alert-success")
 
 @app.route("/mgf/<mgf_name>/spectrum/<spectrum_title>/sequence/<sequence>/mods/<mods>")
 def getSpectrum(mgf_name=None, sequence=None, spectrum_title=None, mods=None):
     global mgf_file_dir
     if mgf_file_dir == "":
-        return "no mgf specified"
+        f = "Please enter the spectrum file(s) folder below."
+        return render_template('tableau.html',response=f, state="alert-danger")
+    if not os.path.isfile(os.path.join(mgf_file_dir,mgf_name)):
+        f = "Could not find '%s' in '%s'.\nDid you specify the spectrum file(s) folder correctly?"%(mgf_name,mgf_file_dir)
+        return render_template('tableau.html',response=f,state="alert-danger")
     modifications = get_varmods(sequence, mods)
     spectrum, charge, parent_mz = get_spectrum_with_mgf(os.path.join(mgf_file_dir,mgf_name), spectrum_title)
     return render_template('spectrum_viewer.html',
@@ -62,7 +79,12 @@ def getSpectrum(mgf_name=None, sequence=None, spectrum_title=None, mods=None):
         spectrum_title = spectrum_title
         )
 
+def open_browser():
+      webbrowser.open_new('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
     #main()
+    Timer(1.5, open_browser).start();
+    print("Please wait, your browser will open soon...")
     app.run()
 
